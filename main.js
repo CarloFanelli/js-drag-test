1778e1c1bdd1cdbc2b70e179ca8cbe9b4089df5a
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-05-31 17:45:24 +0200

    Refactor main.js

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-import DragAndDrop from "./bin/DragAndDrop.js"
+import DragAndDrop from "./bin/DragAndDrop.js";
 
 const dropZones = Array.from(document.querySelectorAll(".dropZone"));
-
 const dragElement = Array.from(document.querySelectorAll(".dragElement"));
-
-new DragAndDrop(
+let dragAndDrop = new DragAndDrop(
   {
     dropZoneHtmlElements: dropZones,
     draggableHtmlElements: dragElement,
-    dropZoneClass: ["bg-pink-500", "border-white-500"], //poco sensato
-    dropZoneHoverClass: "bg-green-400", // sensato
+    dropZoneClasses: ["bg-pink-500", "border-white-500"], //poco sensato
+    dropZoneHoverClasses: ["bg-green-400"],
+    draggedElementHoverClasses: ["bg-blue-400"],
+    draggedElementCloneOpacity: 50,
+    draggedElementClasses: ["bg-yellow-400"],
   },
-  function (e, dropZone) {
-
+  function (element, dropZone) {
     let divResult = document.getElementById("result");
-    console.log(dropZone.id);
-    divResult.innerHTML = `<div>Element draggato: ${e.id}</div> <div> dropzone: ${dropZone.id}`;
+    let elementDragged = "Nessun elemento draggato";
+    let dropZoneHit = "Nessuna drop zone hittata";
+    if (element) {
+      elementDragged = element.id;
+    }
+
+    if (dropZone) {
+      dropZoneHit = dropZone.id;
+    }
+
+    divResult.innerHTML = `<div>Element draggato: ${elementDragged}</div> <div> dropzone: ${dropZoneHit}`;
   }
 );
